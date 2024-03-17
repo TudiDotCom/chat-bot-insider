e15a8f36043152db3034146f62f9812fd17dacb6
tudosecosti13@gmail.com <tudosecosti13@gmail.com>
2024-03-17 21:09:36 +0200

    remove unused import

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,7 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { BOT, USER } from './constants';
 import { HttpClient } from '@angular/common/http'
-import { Observable } from 'rxjs';
 
 @Component({
   selector: 'app-root',
